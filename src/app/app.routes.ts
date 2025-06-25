283c3da756640e3a13fbd9bc69ab496226594345
howyda ahmed <howydaahmedabueila@gmail.com>
2025-06-25 15:22:38 +0300

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', redirectTo: 'home', pathMatch: 'full' },
+   {path:'home',component:HomeComponent,title:'home'},
+   {path:'shop',component:ShopComponent,title:'shop'},
+   {path:'contact',component:ContactComponent,title:'contact'},
+   {path:'blog',component:BlogComponent,title:'blog'},
+   {path:'about',component:AboutComponent,title:'about'},
+];
