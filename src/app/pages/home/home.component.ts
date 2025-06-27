7472c5473bdd398e8b46e6826b62f8b855eb6127
howyda ahmed <howydaahmedabueila@gmail.com>
2025-06-27 11:22:53 +0300

    up

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../service/products.service';
+import { Products } from '../../interface/products';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products?:Products[]
+  productSrv=inject(ProductsService)
+ngOnInit(): void {
+this.getProducts()
+}
+getProducts(){
+  this.productSrv.getAllProducts().subscribe({
+    next :(res:any)=>{
+      this.products = res.data as Products[]
+      console.log(this.products)
 
+    },error :(error)=>console.log('Error Fetching API',error)
+  })
+}
 }
--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
-
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Products } from '../interface/products';
+const apiURL=environment.apiUrl
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-
+private http = inject(HttpClient)
   constructor() { }
+  getAllProducts():Observable<Products>{
+    return this.http.get<Products>(`${apiURL}/api/v1/products`)
+  }
 }
